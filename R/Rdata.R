92bbd8f0bdd0c46b22dca31ddac270ed1dad45f6
asaksager <astridsaksager@gmail.com>
2022-06-15 13:33:22 +0200

    added dataPRA and its documentation
    Former-commit-id: c85eb5a09692055908fb3defc7b1feb113ba4c86

---
+++ R/Rdata.R
@@ -0,0 +1,14 @@
+#' Output example from function Pattern Recognition Analysis
+#'
+#'@description The predicted TSGs and OCGs and their moonlight gene z-score.
+#'
+#'@docType data
+#'@usage data(dataPRA)
+#'@name dataPRA
+#'@aliases dataPRA
+#'@return A list of two.
+#'@format A list of two. 
+#'
+
+#'
+"dataPRA"
