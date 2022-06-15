20a3a550a8bfd34db92d026eb95b7a3e84294e46
asaksager <astridsaksager@gmail.com>
2022-06-15 13:33:22 +0200

    added dataPRA and its documentation

--- R/Rdata.R
+++ R/Rdata.R
@@ -111,3 +111,18 @@
 #'Goldman J., Pollit M., Davis P., Strange A., Ambrose K. and Ciccarelli F.D.
 #'
 "NCG"
+
+#' Output example from function Pattern Recognition Analysis
+#'
+#'@description The predicted TSGs and OCGs and their moonlight gene z-score.
+#'
+#'@docType data
+#'@usage data(dataPRA)
+#'@name dataPRA
+#'@aliases dataPRA
+#'@return A list of two.
+#'@format A list of two. 
+#'
+
+#'
+"dataPRA"
