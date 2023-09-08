143549adcd156ba983421793b4d31bc0a541de86
Matteo Tiberti <matteo.tiberti@gmail.com>
2023-09-08 09:41:07 +0200

    small edits to code style

--- R/PRAtoTibble.R
+++ R/PRAtoTibble.R
@@ -12,15 +12,15 @@
 #' data('dataPRA')
 #' PRAtoTibble(dataPRA)
 PRAtoTibble <- function(dataPRA) {
-  
+
   # Check user input
-  
+
   if (all(names(dataPRA) %in% c("TSG", "OCG")) == FALSE) {
     stop("The two list elements in PRA data must be named TSG and OCG")
   }
-  
+
   # Wrangle data
-  
+
   if (!is.null(dataPRA$TSG)) {
     TSG <- as_tibble(dataPRA$TSG, rownames = NA) %>%
       rownames_to_column(var = "Hugo_Symbol") %>%
@@ -30,7 +30,7 @@ PRAtoTibble <- function(dataPRA) {
   } else {
     TSG <- NULL
   }
-  
+
   if (!is.null(dataPRA$OCG)) {
     OCG <- as_tibble(dataPRA$OCG, rownames = NA) %>%
       rownames_to_column(var = "Hugo_Symbol") %>%
@@ -40,19 +40,19 @@ PRAtoTibble <- function(dataPRA) {
   } else {
     OCG <- NULL
   }
-  
+
   if (!is.null(TSG) & !is.null(OCG)) {
-  drivers <- full_join(TSG, OCG, by = c("Hugo_Symbol",
-                                        "Moonlight_gene_z_score",
-                                        "Moonlight_Oncogenic_Mediator"))
+    drivers <- full_join(TSG, OCG, by = c("Hugo_Symbol",
+                                          "Moonlight_gene_z_score",
+                                          "Moonlight_Oncogenic_Mediator"))
   } else if (is.null(TSG)) {
     drivers <- OCG
   } else if (is.null(OCG)) {
     drivers <- TSG
   }
-  
+
   return(drivers)
-  
+
 }
 
 utils::globalVariables(c("Hugo_Symbol", "value"))
