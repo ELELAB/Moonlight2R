b9b92e009342e7dfc6e8eadccddfcd92d2878293
Katrine Meldgård <katrine@meldgaard.dk>
2024-04-18 16:48:35 +0200

    fixed misspelling of RaSP

--- R/TFinfluence.R
+++ R/TFinfluence.R
@@ -119,7 +119,7 @@ TFinfluence <- function(dataPRA,
     # Keep only the stability classification
     dataMAVISpFiltered <- dataMAVISp |>
                     map(function(x) rename(x, 'stab_class_ros' = matches('Stability classification, [A-Za-z0-9, ]*\\(Rosetta, FoldX\\)( \\[md\\])?'),
-                                              'stab_class_rasp' = matches('Stability classification, [A-Za-z0-9, ]*\\(RasP, FoldX\\)( \\[md\\])?'))) |>
+                                              'stab_class_rasp' = matches('Stability classification, [A-Za-z0-9, ]*\\(RaSP, FoldX\\)( \\[md\\])?'))) |>
                     keep(function(x) 'stab_class_ros' %in% colnames(x) | 'stab_class_rasp' %in% colnames(x)) |>
                     # Choose rosetta consensus, if it is there
                     map(function(x) mutate(x, 'stab_class' = ifelse(test = !('stab_class_ros' %in% colnames(x)),
