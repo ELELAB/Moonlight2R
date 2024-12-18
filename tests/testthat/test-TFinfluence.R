c6d5495f25cde0daa33f199c11b34c35d0455df8
annamelidi <89490095+annamelidi@users.noreply.github.com>
2024-12-18 16:22:24 +0100

    TFinfluence update (#127)
    * TFinfluence update
    
    * update test TFinfluence
    
    ---------
    
    Co-authored-by: Anna Melidi <your@email.com>

--- tests/testthat/test-TFinfluence.R
+++ tests/testthat/test-TFinfluence.R
@@ -7,6 +7,7 @@ data(dataTRRUST)
 data(dataMAF)
 data(dataMAVISp)
 
+
 reference_TFresults <- data.frame(
     Target = c("CHST1", "GEN1", "TAP1", "TAP1"),
     Moonlight_gene_z_score = c(1.06066017177982, 1.30096115353815, 0.8262079533974, 0.8262079533974),
@@ -17,7 +18,9 @@ reference_TFresults <- data.frame(
     PMID = c(NA, NA, "18694960", "15778351"),
     tf_mutation = as.character(c(NA, NA, NA, NA)),
     stab_class = as.character(c(NA, NA, NA, NA)),
-    stringsAsFactors = FALSE # Ensure non-character columns are not treated as factors
+    in_MAVISp = c(FALSE,FALSE,FALSE,FALSE),
+    mutation_available = c(FALSE,FALSE,FALSE,FALSE),
+    stringsAsFactors = FALSE
 )
 
 TFresults <- TFinfluence(dataTRRUST = dataTRRUST,
@@ -29,19 +32,21 @@ TFresults <- TFinfluence(dataTRRUST = dataTRRUST,
 
 character_columns <- c('Target','Moonlight_Oncogenic_Mediator','TF','InteractionType','PMID','tf_mutation','stab_class')
 numeric_columns <- c('Moonlight_gene_z_score','logFC_target')
-all_columns <- c('Target', 'Moonlight_gene_z_score', 'Moonlight_Oncogenic_Mediator', 'logFC_target', 'TF', 'InteractionType', 'PMID', 'tf_mutation', 'stab_class')
+logical_columns <- c('in_MAVISp','mutation_available')
+all_columns <- c('Target', 'Moonlight_gene_z_score', 'Moonlight_Oncogenic_Mediator', 'logFC_target', 'TF', 'InteractionType', 'PMID', 'tf_mutation', 'stab_class','in_MAVISp','mutation_available')
 
 
-# Test that output columns are as expected
-test_that("Output of TFinfluence is a tibble", {
+# Test that output is as expected
+test_that('Output of TFinfluence is a tibble', {
     expect_named(TFresults, all_columns)
     expect_true(all(sapply(TFresults[character_columns], is.character)))
     expect_true(all(sapply(TFresults[numeric_columns], is.numeric)))
+    expect_true(all(sapply(TFresults[logical_columns], is.logical)))
 })
 
 # Test that the content of the output dataframe is as expected
 test_that("Output of TFinfluence does not correspond to reference", {
-    expect_equivalent(TFresults, reference_TFresults)
+    expect_equal(TFresults, reference_TFresults,ignore_attr = TRUE)
 })
 
 
