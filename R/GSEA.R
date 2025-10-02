2f00df36973514d82ac7165d08474885d156f415
Alessia Campo <acampo@kb-bioinfo01.cancer.dk>
2025-10-02 13:43:41 +0200

    Updated /man documents, NAMESPACE and DESCRIPTION

--- R/GSEA.R
+++ R/GSEA.R
@@ -14,6 +14,8 @@
 #' @return return GSEA result
 #' @export
 #' @examples
+#' library(HDO.db)
+#' library(DOSE)
 #' data("DEGsmatrix")
 #' DEGsmatrix_example <- DEGsmatrix[1:2,]
 #' dataFEA <- GSEA(DEGsmatrix = DEGsmatrix_example)
